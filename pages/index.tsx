27a0d44477323cecdf6155a9456d931dd59dba8e
moaaz12-web <moaaz7151@gmail.com>
2023-06-30 23:12:49 +0500

    Removed footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -224,7 +224,7 @@ export default function Home() {
                     placeholder={
                       loading
                         ? 'Waiting for response...'
-                        : 'What is this legal case about?'
+                        : 'What is the condition code MIMA?'
                     }
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
