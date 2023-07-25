67b3b0e7a899bafc5cda850626aa311031902019
hot214 <hot214.dev@gmail.com>
2023-07-26 03:17:42 +0900

    welcome message has been changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about this document?',
+        message: 'How can I help with railroad condition codes?',
         type: 'apiMessage',
       },
     ],
